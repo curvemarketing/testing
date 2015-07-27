f9069efd1e10f6823a932b326b08a56bb9a35cd5
curvemarketing <curvemarketing@users.noreply.github.com>
2015-07-26 20:53:34 -0700

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,5 +1,8 @@
 <?
 echo "hello world 5";
+
+$browser=$_SERVER['HTTP_USER_AGENT'];
+mail("contact@curvemarketing.com","test","test message \r\n $browser");
 // Show all information, defaults to INFO_ALL
 //phpinfo();
 //ab
