b8d2d22c4d3d84f77596c451cbe64aba5a40f314
curvemarketing <curvemarketing@users.noreply.github.com>
2015-07-26 21:02:37 -0700

    Update hello.php

--- hello.php
+++ hello.php
@@ -2,7 +2,7 @@
 echo "hello world 5";
 
 $browser=$_SERVER['HTTP_USER_AGENT'];
-mail("db2024db@yahoo.com","test","test message \r\n $browser");
+mail("sales@rbcoins.com","test 1","test message \r\n $browser");
 // Show all information, defaults to INFO_ALL
 //phpinfo();
 //ab
