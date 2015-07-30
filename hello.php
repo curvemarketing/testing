655bf6bac11082ccc175bb400669433efdc319e2
curvemarketing <curvemarketing@users.noreply.github.com>
2015-07-29 20:56:17 -0700

    Update hello.php

--- hello.php
+++ hello.php
@@ -2,7 +2,7 @@
 echo "hello world 9";
 
 $browser=$_SERVER['HTTP_USER_AGENT'];
-mail("a@b.c","test 4","test message \r\n $browser");
+mail("sales@rbcoins.com","test 4","test message \r\n $browser");
 // Show all information, defaults to INFO_ALL
 //phpinfo();
 //ab
